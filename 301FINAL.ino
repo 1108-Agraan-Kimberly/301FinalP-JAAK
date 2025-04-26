1fb07fb1c239423c819752e7198b31d156d37799
AdrianSaysHi <123914277+AdrianDeli@users.noreply.github.com>
2025-04-25 18:11:49 -0700

    Fixed loophole in waterValue check + Better readability
    - There was an issue with checking the water value where it would constantly be checked even if error was true. Instead, when error is true, it will display a message and do nothing else until the reset button is pressed to go back to idle. Water will be checked only when idle/running is enabled.
    - Instead of having multiple booleans for different states, there is instead a status variable that changes based on different events (that way it is not likely for 2 states being on at the same time). A switch statement reading the status will determine the behavior of the circuit.
    - Made a separate function to check water value to improve readability and efficiency
    * Still need to work on functionality mentioned in the last commit

--- 301FINAL.ino
+++ 301FINAL.ino
@@ -1,21 +1,27 @@
 // Names: Kimberly Agraan, Adrian Delibassis, Aidan Madonna, Joe Wu
-// Project: CPE 302 Final Project
+// Project: CPE 301 Final Project
 
 #include <LiquidCrystal.h>
 
 #define RDA 0x80
 #define TBE 0x20
+
 #define START_BUTTON 5
 #define STOP_BUTTON 6
 #define RESET_BUTTON 7
 #define STEP_BUTTON 4
 
+#define DISABLED 1
+#define IDLE 2
+#define RUNNING 3
+#define ERROR 4
+
 // UART Pointers
-volatile unsigned char *myUCSR0A = (unsigned char*) 0x00C0;
-volatile unsigned char *myUCSR0B = (unsigned char*) 0x00C1;
-volatile unsigned char *myUCSR0C = (unsigned char*) 0x00C2;
-volatile unsigned int  *myUBRR0 = (unsigned int*) 0x00C4;
-volatile unsigned char *myUDR0 = (unsigned char*) 0x00C6;
+volatile unsigned char* myUCSR0A = (unsigned char*) 0x00C0;
+volatile unsigned char* myUCSR0B = (unsigned char*) 0x00C1;
+volatile unsigned char* myUCSR0C = (unsigned char*) 0x00C2;
+volatile unsigned int* myUBRR0 = (unsigned int*) 0x00C4;
+volatile unsigned char* myUDR0 = (unsigned char*) 0x00C6;
 
 // ADC Pointers
 volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
@@ -27,11 +33,6 @@ volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 volatile unsigned char* port_b = (unsigned char*) 0x25; 
 volatile unsigned char* ddr_b = (unsigned char*) 0x24; 
 volatile unsigned char* pin_b = (unsigned char*) 0x23; 
-
-bool disabled = true;
-bool idle = false;
-bool running = false;
-bool error = false;
 
 void setup()
 {
@@ -53,56 +54,69 @@ void setup()
 
 void loop()
 {
+  int status = DISABLED;
+
   // start button
-  if(*pin_b & (1 << START_BUTTON))
+  if(*pin_b & (1 << START_BUTTON) && status == DISABLED)
   {
-    idle = true;
-    disabled = false;
+    status = IDLE;
   }
   // stop button
   if(*pin_b & (1 << STOP_BUTTON)) 
   {
-    diabled = true;
-    idle = running = error = false;
+    status = DISABLED;
   }
   // reset button
-  if(*pin_b & (1 << RESET_BUTTON))
+  if(*pin_b & (1 << RESET_BUTTON) && status == ERROR)
   {
-    idle = true;
-    error = false;
+    status = IDLE;
   }
 
-  if(!disabled)
+  switch(status)
   {
-    int waterValue = adc_read(0);
-    if(waterValue > 220) // subject to change
-    {
-      // idle or running
-      // detect temp
-    }
-    else if(waterValue <= 220) // subject to change
-    {
+    // disabled = yellow LED
+    case(DISABLED):
+      // TURN LED ON
+      break;
+    // idle = green LED
+    case(IDLE):
+      // TURN LED ON
+      if(checkWaterStatus(status))
+      {
+
+      }
+      break;
+    // running = blue LED
+    case(RUNNING):
+      // TURN LED ON
+      if(checkWaterStatus(status))
+      {
+
+      }
+      break;
+    // error = red LED
+    case(ERROR):
+      // TURN LED ON
       char message[] = "Water level is too low";
       for(int i = 0; message[i] != '\0'; i++)
         U0putchar(message[i]);
       U0putchar('\n');
-      error = true;
-      idle = running = false;
-    }
+      break;
   }
+}
 
-  // disabled = yellow LED
-  if(disabled)
-    ;
-  // idle = green LED
-  if(idle)
-    ;
-  // running = blue LED
-  if(running)
-    ;
-  // error = red LED
-  if(error)
-    ;
+bool checkWaterStatus(int& status)
+{
+  int waterValue = adc_read(0);
+  if(waterValue > 220)  // threshold subject to change
+  {
+    return true;
+  }
+  else if(waterValue <= 220)  // threshold subject to change
+  {
+    status = ERROR;
+    return false;
+  }
 }
 
 // UART functions
@@ -147,19 +161,13 @@ void adc_init()
   *my_ADMUX &= 0b11011111;
   *my_ADMUX &= 0b11110000;
 }
-unsigned int adc_read(unsigned char adc_channel_num) //work with channel 0
+unsigned int adc_read(unsigned char adc_channel_num)
 {
-  // clear the channel selection bits (MUX 4:0)
   *my_ADMUX &= 0b11110000;
-  // clear the channel selection bits (MUX 5) hint: it's not in the ADMUX register
   *my_ADCSRB &= 0b11110111;
-  // set the channel selection bits for channel 0
   *my_ADMUX += 0;
-  // set bit 6 of ADCSRA to 1 to start a 
   *my_ADCSRA |= 0b01000000;
-  // wait for the conversion to complete
   while(*my_ADCSRA & 0x40);
-  // return the result in the ADC data register and format the data based on right justification (check the lecture slide)
   unsigned int val = (*my_ADC_DATA & 0x03FF);
   return val;
 }
